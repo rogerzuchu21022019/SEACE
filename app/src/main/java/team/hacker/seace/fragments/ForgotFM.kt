c1442f6773d0bac013fe40ae4da1dec5e2793950
QuanVP <quanvp2021@gmail.com>
2022-03-24 22:14:50 +0700

    updateNavController

--- app/src/main/java/team/hacker/seace/fragments/ForgotFM.kt
+++ app/src/main/java/team/hacker/seace/fragments/ForgotFM.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import team.hacker.seace.R
+import team.hacker.seace.databinding.FragmentForgotFmBinding
 
 
 class ForgotFM : Fragment() {
-
+    lateinit var forgotFmBinding: FragmentForgotFmBinding
+    lateinit var navController : NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_fm, container, false)
+        forgotFmBinding = FragmentForgotFmBinding.inflate(layoutInflater)
+        forgotFmBinding.btnOK.setOnClickListener{
+            navController = findNavController()
+            var action = ForgotFMDirections.actionForgotFMToSignInFM()
+            navController.navigate(action)
+        }
+        return forgotFmBinding.root
     }
 
 }
--- app/src/main/java/team/hacker/seace/fragments/SignInFM.kt
+++ app/src/main/java/team/hacker/seace/fragments/SignInFM.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import team.hacker.seace.R
 import team.hacker.seace.databinding.FragmentSignInFmBinding
 
 class SignInFM : Fragment() {
+    lateinit var navController: NavController
     lateinit var signInFmBinding: FragmentSignInFmBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +19,19 @@ class SignInFM : Fragment() {
     ): View? {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in_fm, container, false)
+        signInFmBinding = FragmentSignInFmBinding.inflate(layoutInflater)
+
+        signInFmBinding.txtRegister.setOnClickListener{
+            navController = findNavController()
+            var action = SignInFMDirections.actionSignInFMToRegisterFM()
+            navController.navigate(action)
+        }
+
+        signInFmBinding.txtForgotPassword.setOnClickListener{
+            navController = findNavController()
+            var action = SignInFMDirections.actionSignInFMToForgotFM()
+            navController.navigate(action)
+        }
+        return signInFmBinding.root
     }
 }
