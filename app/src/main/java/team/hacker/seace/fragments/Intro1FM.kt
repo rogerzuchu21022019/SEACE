c02f16269017814ebc2a68755750a02d8ebe6657
QuanVP <quanvp2021@gmail.com>
2022-03-25 11:43:45 +0700

    home & profile

--- app/src/main/java/team/hacker/seace/fragments/Intro1FM.kt
+++ app/src/main/java/team/hacker/seace/fragments/Intro1FM.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import team.hacker.seace.R
+import team.hacker.seace.databinding.FragmentIntro1FmBinding
 
 class Intro1FM : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var intro1FmBinding: FragmentIntro1FmBinding
+    lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro1_fm, container, false)
+        intro1FmBinding = FragmentIntro1FmBinding.inflate(layoutInflater)
+        return intro1FmBinding.root
     }
 }
--- app/src/main/java/team/hacker/seace/fragments/Intro2FM.kt
+++ app/src/main/java/team/hacker/seace/fragments/Intro2FM.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import team.hacker.seace.R
+import team.hacker.seace.databinding.FragmentIntro2FmBinding
+import team.hacker.seace.databinding.FragmentIntro3FmBinding
 
 class Intro2FM : Fragment() {
+    lateinit var intro2FmBinding: FragmentIntro2FmBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro2_fm, container, false)
+        intro2FmBinding = FragmentIntro2FmBinding.inflate(layoutInflater)
+        return intro2FmBinding.root
     }
 }
