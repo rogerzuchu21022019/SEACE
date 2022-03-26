f4203235fc780e5b5ec82f4144a71d7c17155e71
vunam <vuthanhnam21022019@gmail.com>
2022-03-26 17:45:18 +0700

    Nam Update Layout HomeFB

--- app/src/main/java/team/hacker/seace/fragments/homefacebook/HomeScreenFM.kt
+++ app/src/main/java/team/hacker/seace/fragments/homefacebook/HomeScreenFM.kt
@@ -1,4 +1,4 @@
-package team.hacker.seace.fragments
+package team.hacker.seace.fragments.homefacebook
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,13 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import team.hacker.seace.R
+import team.hacker.seace.databinding.FragmentHomeScreenFmBinding
 
 class HomeScreenFM : Fragment() {
+    lateinit var fmHomeScreenFmBinding: FragmentHomeScreenFmBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen_fm, container, false)
+        fmHomeScreenFmBinding = FragmentHomeScreenFmBinding.inflate(layoutInflater)
+        return fmHomeScreenFmBinding.root
     }
 }
